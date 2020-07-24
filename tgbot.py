6839b4c4052eb77596cdf478e3c3a8df3c0432d9
Pasha Suprunchuk <vipnoth@pm.me>
2020-07-24 13:48:29 +0300

    TGbot

---
+++ tgbot.py
@@ -0,0 +1,17 @@
+import telebot
+bot = telebot.TeleBot("token")
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, как тебя ховут?')
+ 
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'Алексей':
+        bot.send_message(message.chat.id, 'Какое-то у тебя плохое имя!')
+    elif message.text == 'Станислав':
+        bot.send_message(message.chat.id, 'Хорошее имя')      
+    else:
+        bot.send_message(message.chat.id, 'Введите имя Станислав либо Алексей')
+
+bot.polling(none_stop=True)
